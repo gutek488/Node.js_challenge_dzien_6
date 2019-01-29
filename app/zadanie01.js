cd54a00a93b4423c7cecc03f0609d52cf1572a32
Gustaw <gutek488@gmail.com>
2019-01-29 17:23:52 +0100

    Task 1 and 2 completed

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,22 @@
-//Twój kod
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.urlencoded());
+app.use(express.static('./public/zadanie01/'));
+
+
+app.post('/wynik', (req, res) => {
+    const {num1, num2} = req.body;
+    if(parseInt(num1) % parseInt(num2) === 0) {
+        res.send(`Liczba ${num1} jest podzielna przez ${num2}`);
+    } else {
+        res.send(`Liczba ${num1} nie jest podzielna ${num2}`);
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Serwer ustawiony na porcie 3000');
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,39 @@
-//Twój kod
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.urlencoded());
+app.use(express.static('./public/zadanie02/'));
+
+
+app.post('/cookie/set', (req, res) => {
+    const {name} = req.body;
+    res.cookie('name', `${name}`, {
+        maxAge : 2628000000,
+    });
+    res.send('Zapisano imię!');
+});
+
+
+
+app.get('/cookie/show', (req, res) => {
+    const myCookie = req.cookies.name;
+    res.send('Zapisane imię to: ' + myCookie);
+});
+
+app.get('/cookie/test', (req, res) => {
+    const myCookie = req.cookies.name;
+    if (myCookie === undefined) {
+        res.send("Imię nie zostało zapisane.")
+    } else {
+        res.send("Imię zostało zapisane")
+    }
+
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Serwer ustawiony na porcie 3000');
+});
